3cca57999eb715b8cf0529d60a40c907340288d2
yoch <yoch.melka@gmail.com>
2017-04-14 12:21:35 +0300

    change plots

--- scripts/plot.py
+++ scripts/plot.py
@@ -23,19 +23,23 @@ if __name__ == '__main__':
         # sort word pairs by similarity
         order = np.argsort(gold)
 
-        fig = plt.figure()
-
-        plt.ylim((-0.1,4.1))
-
-        plt.plot(gold[order], '-', c='0.45', label='gold')
-        plt.plot(run1[order], '+', c='0.4', ms=4, label='run 1')
-        plt.plot(run2[order], 'x', c='0.15', ms=3, label='run 2')
+        fig, axes = plt.subplots(2, figsize=(8, 10), sharex=True, sharey=True)
 
         plt.ylabel('score')
         plt.xlabel('word pairs')
+        
+        plt.ylim((-0.1,4.1))
+        #plt.title(key)
 
-        plt.legend(loc='best')
-        plt.title(key)
+        axes[0].plot(gold[order], '-', c='0.1', label='gold')
+        axes[0].plot(run1[order], '+', c='0.3', ms=5.5, label='version 1')
+        axes[0].legend(loc='best')
 
-        plt.savefig('../misc/plot-%s.png' % key)
+        axes[1].plot(gold[order], '-', c='0.1', label='gold')
+        axes[1].plot(run2[order], 'x', c='0.2', ms=4.5, label='version 2')
+        axes[1].legend(loc='upper left')
+
+        #fig.tight_layout()
+        plt.subplots_adjust(wspace=0.15, hspace=0.15)
+        plt.savefig('../misc/plot-%s.pdf' % key, bbox_inches="tight")
         plt.close()
